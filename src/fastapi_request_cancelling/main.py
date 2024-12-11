fbb1710e73793ad087536bf0c50e97927d2097c1
Fabio Kapsahili <fabio.kapsahili1@gmail.com>
2024-12-11 11:22:47 +0100

    Switch to using event_publisher using anyio

--- src/fastapi_request_cancelling/main.py
+++ src/fastapi_request_cancelling/main.py
@@ -2,7 +2,8 @@ import asyncio
 import uvicorn
 import json
 from fastapi import FastAPI, Request
-from fastapi.responses import StreamingResponse
+from sse_starlette import EventSourceResponse
+import anyio
 
 app = FastAPI()
 
@@ -10,31 +11,38 @@ def get_sse_packet(json_data: str):
     """Get an SSE packet."""
     return f"data: {json_data}\n\n"
 
+
+async def handle(send_chan):
+    async with send_chan:
+        try:
+            for i in range(100):
+                await send_chan.send(i)
+                await asyncio.sleep(1)
+        except anyio.ClosedResourceError:
+            print("Closed!")
+
 # Completions endpoint
 @app.get("/stream")
 async def stream_output(request: Request):
-    async def generator():
-        try:
-            for i in range(10):
-                if await request.is_disconnected():
-                    print("Disconnect triggered!")
-                    break
+    send_chan, recv_chan = anyio.create_memory_object_stream(100)
 
-                await asyncio.sleep(1)
+    async def event_publisher():
+        async with send_chan:
+            task = asyncio.create_task(handle(send_chan))
+            try:
+                async for msg in recv_chan:
+                    yield get_sse_packet(json.dumps({'testnum': msg}))
+                await task
+                if task.exception():
+                    raise Exception("Task failed!")
+            except asyncio.CancelledError:
+                print("Cancelled exception triggered!")
+            except Exception as e:
+                print(
+                    "Exception triggered!", e
+                )
+    return EventSourceResponse(event_publisher())
 
-                yield get_sse_packet(json.dumps({'testnum': i}))
-        except asyncio.CancelledError:
-            # Here, the print gets logged, then the exception gets sent as well
-            print("Cancelled exception triggered!")
-        except Exception:
-            print(
-                "Exception triggered!"
-            )
-
-    return StreamingResponse(
-        generator(),
-        media_type="text/event-stream",
-    )
 
 
 def start():
